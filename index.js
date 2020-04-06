85d0324cf03e6d2289e114233779a1067b632861
Pergamene <smartz373@gmail.com>
2020-04-06 12:52:56 -0600

    method stubs

--- index.js
+++ index.js
@@ -2,5 +2,37 @@ const express = require("express");
 
 const server = express();
 
+let users = [
+  {
+    id: 1,
+    name: 'Jane Doe',
+    bio: "Not Tarzan's Wife, another Jane",
+  },
+];
+
+// middlware
+server.use(express.json());
+
+// end points
+server.get('/api/users', (req, res) => {
+
+});
+
+server.get('/api/users/:id', (req, res) => {
+
+});
+
+server.post('/api/users', (req, res) => {
+
+});
+
+server.delete('/api/users/:id', (req, res) => {
+
+});
+
+server.patch('/api/users/:id', (req, res) => {
+
+});
+
 const port = 5000;
 server.listen(port, () => console.log(`\n== api on port ${port} ==\n`));
